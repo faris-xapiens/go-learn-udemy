2401e8a840bb8268d320872259ed8a163cb51fbe
Faris Adlin <farisnura@gmail.com>
2021-11-29 15:20:18 +0700

    Lesson 6 decision structures

--- main.go
+++ main.go
@@ -2,52 +2,16 @@ package main
 
 import "log"
 
-// type User struct {
-// 	FirstName string
-// 	LastName  string
-// }
-
-// func main() {
-// 	myMap := make(map[string]User)
-// 	me := User{
-// 		FirstName: "Trevor",
-// 		LastName:  "Sawler",
-// 	}
-
-// 	myMap["me"] = me
-
-// 	log.Println(myMap["me"].FirstName)
-
-// 	var myNewVar float32
-
-// 	myNewVar = 11.1
-
-// 	log.Println(myNewVar)
-
-// }
-
-// func main() {
-// 	var mySlice []string
-
-// 	mySlice = append(mySlice, "Faris")
-
-// 	mySlice = append(mySlice, "John")
-// 	mySlice = append(mySlice, "Ade")
-// 	mySlice = append(mySlice, "Indra")
-
-// 	sort.Strings(mySlice)
-
-// 	log.Println(mySlice)
-
-// }
-
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	names := []string{"Faris", "Ade", "Sunhaki"}
+	myVar := "asga"
 
-	log.Println(numbers)
+	switch myVar {
+	case "cat":
+		log.Println("cat is set to cat")
+	case "dog":
+		log.Println("cat is set to cat")
+	default:
+		log.Println("Not found")
 
-	log.Println(numbers[0:2])
-
-	log.Println(names)
+	}
 }
