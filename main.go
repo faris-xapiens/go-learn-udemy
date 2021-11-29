b9627319bdc39262b5365e1274da40d2ce4bb935
Faris Adlin <farisnura@gmail.com>
2021-11-29 18:51:15 +0700

    Lesson 10 reading json

--- main.go
+++ main.go
@@ -1,24 +1,68 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-
-	"github.com/faris-xapiens/go-learn-udemy/helpers"
 )
 
-const numPool = 10
-
-func calculateValue(intChan chan int) {
-	randomNumber := helpers.RandomNumber(numPool)
-	intChan <- randomNumber
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	HairColor string `json:"hair_color"`
+	HasDog    bool   `json:"has_dog"`
 }
 
 func main() {
-	intChan := make(chan int)
-	defer close(intChan)
+	myJson := `
+	[
+		{
+			"first_name": "Ade",
+			"last_name": "Sunhaki",
+			"hair_color": "Grey",
+			"has_dog": true
+		},
+		{
+			"first_name": "Kamil",
+			"last_name": "Sunhaki",
+			"hair_color": "Red",
+			"has_dog": false
+		}
+	]`
 
-	go calculateValue(intChan)
+	var unmarshalled []Person
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
 
-	num := <-intChan
-	log.Println(num)
+	if err != nil {
+		log.Println("Error unmarshalling json", err)
+	}
+
+	log.Println("unmarshalled: %v", unmarshalled)
+
+	var mySlice []Person
+
+	var m1 Person
+	m1.FirstName = "Wally"
+	m1.LastName = "West"
+	m1.HairColor = "Red"
+	m1.HasDog = false
+
+	mySlice = append(mySlice, m1)
+
+	var m2 Person
+	m2.FirstName = "Michael"
+	m2.LastName = "East"
+	m2.HairColor = "Purple"
+	m2.HasDog = true
+
+	mySlice = append(mySlice, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "    ")
+
+	if err != nil {
+		log.Println("error marshalling", err)
+	}
+
+	fmt.Println(string(newJson))
+
 }
