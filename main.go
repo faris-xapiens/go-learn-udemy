e20896ea9d94b5349e46b792d189e4bb17b12114
Faris Adlin <farisnura@gmail.com>
2021-11-29 15:43:53 +0700

    Lesson 8 interface

--- main.go
+++ main.go
@@ -1,30 +1,56 @@
 package main
 
-import "log"
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
 
 func main() {
-	type User struct {
-		Firstname string
-		Lastname  string
-		Email     string
-		Age       int
+	dog := Dog{
+		Name:  "Ade",
+		Breed: "Sunhaki Clan",
 	}
 
-	var users []User
+	PrintInfo(&dog)
 
-	// var users []User
-	users = append(users, User{"Ade", "Sunhaki", "ade.sunhaki@gmail.com", 30})
-
-	log.Println(users)
-
-	// firstLine := "Once upon a midnight dreary"
-
-	// for animalType, animal := range firstLine {
-	// 	log.Println(animalType, ":", animal)
-	// }
-
-	for _, l := range users {
-		log.Println(l.Firstname, l.Lastname)
+	gorilla := Gorilla{
+		Name:          "Jock",
+		Color:         "grey",
+		NumberOfTeeth: 123,
 	}
 
+	PrintInfo(&gorilla)
+}
+
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+func (d *Dog) Says() string {
+	return "Woof"
+}
+
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
+
+func (d *Gorilla) Says() string {
+	return "Ugh"
+}
+
+func (d *Gorilla) NumberOfLegs() int {
+	return 2
 }
