7cc8e4a601e3808b419c284c914302b4d6d0b1e9
Faris Adlin <farisnura@gmail.com>
2021-11-29 14:35:48 +0700

    Learn Map Slices again

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 // type User struct {
 // 	FirstName string
 // 	LastName  string
@@ -23,3 +25,29 @@ package main
 // 	log.Println(myNewVar)
 
 // }
+
+// func main() {
+// 	var mySlice []string
+
+// 	mySlice = append(mySlice, "Faris")
+
+// 	mySlice = append(mySlice, "John")
+// 	mySlice = append(mySlice, "Ade")
+// 	mySlice = append(mySlice, "Indra")
+
+// 	sort.Strings(mySlice)
+
+// 	log.Println(mySlice)
+
+// }
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	names := []string{"Faris", "Ade", "Sunhaki"}
+
+	log.Println(numbers)
+
+	log.Println(numbers[0:2])
+
+	log.Println(names)
+}
